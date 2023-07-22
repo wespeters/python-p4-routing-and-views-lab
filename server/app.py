6cd854f5855b1c8d16710bc4bdae562489b0b436
Wesley Peters <wesleypeters@gmail.com>
2023-07-22 13:33:08 -0600

    add routes and views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string>')
+def print_string(string):
+    print(string)
+    return string
+
+@app.route('/count/<int:num>')
+def count(num):
+    return '\n'.join(str(i) for i in range(num)) + '\n'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Operation not supported.", 400
+
+    return str(result)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
